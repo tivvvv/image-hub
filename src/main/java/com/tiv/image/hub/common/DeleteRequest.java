d934356bd2db8f39c5c50f7e13415778d9306050
tivvvv <18612553935@163.com>
2025-06-19 07:30:28 +0800

    feat: 通用请求封装.

---
+++ src/main/java/com/tiv/image/hub/common/DeleteRequest.java
@@ -0,0 +1,20 @@
+package com.tiv.image.hub.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 通用删除请求类
+ */
+@Data
+public class DeleteRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+
+}
