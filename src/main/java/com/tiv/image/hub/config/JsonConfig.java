bd3b2105193cdd355c8a118c3eccdabbfead4f54
tivvvv <18612553935@163.com>
2025-06-30 08:36:17 +0800

    feat: 解决long类型精度丢失问题.

---
+++ src/main/java/com/tiv/image/hub/config/JsonConfig.java
@@ -0,0 +1,29 @@
+package com.tiv.image.hub.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * SpringMVC Json配置
+ */
+@JsonComponent
+public class JsonConfig {
+
+    /**
+     * 解决long类型精度丢失问题
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
+
+}
