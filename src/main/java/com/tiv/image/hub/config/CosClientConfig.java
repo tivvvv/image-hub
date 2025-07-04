15c901477de6f0fbaad5907595ab823c02a9bcf9
tivvvv <18612553935@163.com>
2025-07-05 00:29:52 +0800

    feat: 配置腾讯云对象存储.

---
+++ src/main/java/com/tiv/image/hub/config/CosClientConfig.java
@@ -0,0 +1,56 @@
+package com.tiv.image.hub.config;
+
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.http.HttpProtocol;
+import com.qcloud.cos.region.Region;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 腾讯云对象存储配置.
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "cos.client")
+public class CosClientConfig {
+
+    /**
+     * 域名
+     */
+    private String host;
+
+    /**
+     * secretId
+     */
+    private String secretId;
+
+    /**
+     * secretKey
+     */
+    private String secretKey;
+
+    /**
+     * 区域
+     */
+    private String region;
+
+    /**
+     * 桶名
+     */
+    private String bucket;
+
+    @Bean
+    public COSClient cosClient() {
+        // https://console.cloud.tencent.com/cam/capi
+        COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
+        ClientConfig clientConfig = new ClientConfig(new Region(region));
+        clientConfig.setHttpProtocol(HttpProtocol.https);
+        return new COSClient(cred, clientConfig);
+    }
+
+}
