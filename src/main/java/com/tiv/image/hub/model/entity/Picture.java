0bebb823edfaf2dde94a086e008719087b04abc6
tivvvv <18612553935@163.com>
2025-07-15 12:07:27 +0800

    fix: 修改图片id为assign id.

--- src/main/java/com/tiv/image/hub/model/entity/Picture.java
+++ src/main/java/com/tiv/image/hub/model/entity/Picture.java
@@ -24,7 +24,7 @@ public class Picture implements Serializable {
     /**
      * 图片id
      */
-    @TableId(value = "id", type = IdType.AUTO)
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
     private Long id;
 
     /**
